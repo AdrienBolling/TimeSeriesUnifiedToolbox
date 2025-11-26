cfe9f8a223bde4234a0987b41b426c8fc157c317
Adrien Bolling <bolling.adrien@gmail.com>
2025-11-26 13:56:50 +0100

    feat: base typing interface for the library
    Added a generic "Data" type
    Added a "TimeSeries" interface
    TODO: implement common functions over TimeSeries, define the storage
    logic, subclass with Univariate and Multivariate Time Seriess

--- src/tsut/core/common/data/types.py
+++ src/tsut/core/common/data/types.py
@@ -1,18 +1,25 @@
 """Define the base Data types for the TSUT Framework."""
 
-from typing import Any
+from typing import Any, NamedTuple, TypeVar
 
 import numpy as np
 import pandas as pd
+from pydantic import BaseModel
+
+D = TypeVar("D", bound="Data")
+K = TypeVar("K")
 
 
-class BaseData:
+type ContextData = dict[str, Any] | BaseModel | NamedTuple
+
+
+class Data:
     """Base class for all data types in the TSUT Framework."""
 
-    # INFO : For now there isn't any specifics that need to be defined across all data types.
+    # INFO : For now there isn't any specifics that need to be defined across all data types. It is intentional, this type is only used to ensure all inputs/outputs come from the TSUT library.
 
 
-class TimeSeries(BaseData):
+class TimeSeries(Data):
     """Class representing time series data.
 
     The implementation is highly inspired from the Darts TimeSeries class. It is intentionally more lightweight however.
