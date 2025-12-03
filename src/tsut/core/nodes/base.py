4637df7afb014742a25f092bc8ea542661cf6d8e
Adrien Bolling <bolling.adrien@gmail.com>
2025-12-03 14:55:41 +0100

    feat: add post_init hook to any class inheriting from Node
    metaclass implementation to ensure a proper call to a post init hook,
    that encapsulates logic such as setting the config as a class attribute
    and ensuring call to super().__init__ to not break the inheritance chain

--- src/tsut/core/nodes/base.py
+++ src/tsut/core/nodes/base.py
@@ -1,15 +1,19 @@
 """Define the atomic component of a TSUT Pipeline: the Node."""
 
-from abc import ABC, abstractmethod
+import uuid
+from abc import ABC, ABCMeta, abstractmethod
 from enum import StrEnum
-from typing import Any, TypeVar
+from typing import Any, ParamSpec, TypeVar, override
 
-from pydantic import BaseModel
+from pydantic import BaseModel, PrivateAttr
 
 from tsut.core.common.data.types import ContextData, Data
+from tsut.core.common.mixins.base import MixinSettings
 
 D_I = TypeVar("D_I", bound=Data)
 D_O = TypeVar("D_O", bound=Data)
+
+P = ParamSpec("P")
 
 
 class NodeType(StrEnum):
@@ -32,12 +36,38 @@ class Port(BaseModel):
 class NodeConfig(BaseModel):
     """Configuration for a Node in a TSUT Pipeline."""
 
+    _id: uuid.UUID = PrivateAttr(uuid.uuid4())
     node_type: NodeType = NodeType.BASE
     in_ports: dict[str, Port] = {}
     out_ports: dict[str, Port] = {}
+    mixins_config: dict[str, MixinSettings] = {}
+
+    @override
+    def __hash__(self) -> int:
+        """Hash the NodeConfig based on its UUID. Assume that two Nodes with the same config can be considered different."""
+        return hash(self._id)
+
+    @property
+    def id(self) -> uuid.UUID:
+        """Property to get the unique identifier of the NodeConfig."""
+        return self._id
 
 
-class Node[D_I, D_O](ABC):
+class MetaPostInitHook(ABCMeta):
+    """Metaclass to add an automatic call to a '__post_init__' method after '__init__'."""
+
+    def __call__(cls, *args: Any, **kwargs: Any) -> Any:
+        """Add automatic call to 'post_init' hook after '__init__'."""
+        # Before we do __new__ and __init__
+        instance: object = super().__call__(*args, **kwargs)
+        # After we do __post_init__
+        post_hook = getattr(instance, "__post_init__", None)
+        if callable(post_hook):
+            _ = post_hook(*args, **kwargs)
+        return instance
+
+
+class Node[D_I, D_O](ABC, metaclass=MetaPostInitHook):
     """Base class for a Node in a TSUT Pipeline."""
 
     _is_node: bool = True
@@ -51,8 +81,11 @@ class Node[D_I, D_O](ABC):
         self.in_ports: dict[str, Port] = config.in_ports
         self.out_ports: dict[str, Port] = config.out_ports
 
-    def __init_subclass__(cls, **kwargs: Any) -> None:  # noqa: ANN401
-        """Ensure that all subclasses of Node are placed before Mixin classes in the inheritance order. (To ensure proper initialization order)."""
+    def __init_subclass__(cls, *args: ParamSpec, **kwargs: ParamSpec) -> None:
+        """Ensure several things.
+
+        - That all subclasses of Node are placed before Mixin classes in the inheritance order. (To ensure proper initialization order).
+        """
         super().__init_subclass__(**kwargs)
 
         bases = cls.__bases__
@@ -69,8 +102,31 @@ class Node[D_I, D_O](ABC):
                 message += f"Use 'class {cls.__name__}({bases[base_idx].__name__}, {base.__name__}):' instead of 'class {cls.__name__}({base.__name__}, {bases[base_idx].__name__}):'."
                 raise TypeError(message)
 
+    def __post_init__(self, *args: Any, **kwargs: Any) -> None:
+        """Define common post-initialization hook for all Nodes.
+
+        If 'config' is passed as kwarg, set it as the Node's '_config' attribute.
+        Ensure a call to super().init() with the proper args.
+        """
+        if "config" not in kwargs:
+            message = "Node must be initialized with a 'config' keyword argument, subtype of 'NodeConfig'."
+            raise ValueError(message)
+        self._config: NodeConfig = kwargs["config"]
+        super().__init__(*args, **kwargs)
+        # If super has a post_init, call it
+        if getattr(
+            super(), "__post_init__", None
+        ):  # INFO: Ignore the error here if pyright raises one, it's being dumb
+            super().__post_init__(*args, **kwargs)
+            # INFO: Although it may seem useless, it's used in case of multiple inheritance with Mixins and such
+
+    @property
+    def id(self) -> uuid.UUID:
+        """Get the unique identifier of the Node based on its configuration."""
+        return self._config.id
+
     @abstractmethod
-    def node_fit(self, data: dict[str, D_I | ContextData] | None) -> None:
+    def node_fit(self, data: dict[str, D_I | ContextData]) -> None:
         """Define the base logic for fitting a Node with the given data. Can also be called with  no data to implement setup logic."""
         raise NotImplementedError
 
