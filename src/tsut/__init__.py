c7a8728ac2dccb8f7cabe2dcf91e7346eb5a38da
Adrien Bolling <bolling.adrien@gmail.com>
2026-03-30 01:33:46 +0200

    updated depedencies

---
+++ src/tsut/__init__.py
@@ -0,0 +1,9 @@
+### Start auto-discovery of components in the components package.
+from tsut.core.nodes.registry.node_registry import NODE_REGISTRY as NODE_REGISTRY
+print(NODE_REGISTRY)
+
+if NODE_REGISTRY is None:
+    raise ImportError("NODE_REGISTRY is not defined. Please ensure that the node registry is properly implemented and imported.")
+from tsut.components import _auto_discovery
+
+_auto_discovery()
