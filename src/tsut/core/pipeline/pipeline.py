cfb88a0ebfa49b8e7a571729fc96f9a09b7ee6e6
Adrien Bolling <bolling.adrien@gmail.com>
2026-03-30 01:33:33 +0200

    feat : added metrics to pipelines as well as some convenience and validation capabilities, needs more enforcement

--- src/tsut/core/pipeline/pipeline.py
+++ src/tsut/core/pipeline/pipeline.py
@@ -12,8 +12,9 @@ import networkx as nx
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings
 
-from tsut.core.common.data.types import Data
-from tsut.core.nodes.node import Node, NodeConfig
+from tsut.core.common.data.data import Data
+from tsut.core.nodes.metrics.metric_node import MetricNodeConfig
+from tsut.core.nodes.node import Node, NodeConfig, NodeType, Port
 from tsut.core.nodes.registry.node_registry import NODE_REGISTRY
 from tsut.core.pipeline.render import plot_graph_with_metadata
 
@@ -50,11 +51,10 @@ class Edge(BaseModel):
     target: str
     ports_map: dict[str, str]  # Input port of target node to output port of source node
 
-
 class PipelineConfig(BaseModel):
     """Define the global configuration for a TSUT Pipeline (Not the layout)."""
 
-    nodes: dict[str, tuple[str, NodeConfig]] = {}
+    nodes: dict[str, tuple[str, NodeConfig | None]] = {}
     edges: list[Edge] = []
     settings: PipelineSettings = PipelineSettings()
 
@@ -92,7 +92,7 @@ class Pipeline:
 
         # --- Initialize the core attributes of the Pipeline
         # Will be updated during compilation of the pipeline, and used during execution by the runners.
-        self.node_objects: dict[str, Node[Data, Data]] = {}
+        self.node_objects: dict[str, Node] = {}
         # Create the inner nx graph for representation and graph-related operations
         self._init_graph_repr()
         # Check if there are any nodes, if so initialize them
@@ -117,7 +117,7 @@ class Pipeline:
         return self._config.settings
 
     @property
-    def nodes(self) -> dict[str, tuple[str, NodeConfig]]:
+    def nodes(self) -> dict[str, tuple[str, NodeConfig | None]]:
         """Get the pipeline nodes configuration."""
         return self._config.nodes
 
@@ -132,6 +132,18 @@ class Pipeline:
         return self._graph
 
     @property
+    def graph_wo_metrics(self) -> nx.DiGraph:
+        """Get the internal graph representation of the pipeline without metric edges."""
+        def filter_node(n: str) -> bool:
+            # Filter out metric nodes
+            return self.config.nodes[n][1].node_type != "metric"
+
+        def filter_edge(u: str, v: str) -> bool:
+            # Filter out edges connected to metric nodes
+            return not filter_node(u) or not filter_node(v)
+        return nx.subgraph_view(self.graph, filter_node=filter_node, filter_edge=filter_edge)
+
+    @property
     def compiled(self) -> bool:
         """Check if the pipeline is compiled.
 
@@ -143,7 +155,7 @@ class Pipeline:
 
     # --- Public API for pipeline management (adding/removing nodes and edges, compiling, etc.)
 
-    def add_node(self, node_class_name: str,node_name: str, node_config: NodeConfig) -> None:
+    def add_node(self, node_class_name: str,node_name: str, node_config: NodeConfig | None) -> None:
         """Add a node to the pipeline."""
         if node_name in self._config.nodes:
             message = f"Node with name '{node_name}' already exists in the pipeline."
@@ -220,15 +232,68 @@ class Pipeline:
         )
         plt.plot
 
-    # --- Internal methods for node instantiation and management
+    def get_params(self) -> dict[str, dict[str, Any]]:
+        """Get the aggregate of all parameters of the nodes in the pipeline"""
+        params = {}
+        for node_name, node in self.node_objects.items():
+            params[node_name] = node.get_params() if hasattr(node, "get_params") else {}
+        return params
+
+    def set_params(self, params: dict[str, dict[str, Any]]) -> None:
+        """Set the parameters of the nodes in the pipeline.
+
+        Args:
+            params (dict[str, dict[str, Any]]): A dictionary where keys are node names and values are dictionaries of parameters to set for each node.
+
+        Returns:
+            None
+
+        """
+        for node_name, node_params in params.items():
+            if node_name not in self.node_objects:
+                message = f"Node with name '{node_name}' does not exist in the pipeline."
+                raise ValueError(message)
+            node = self.node_objects[node_name]
+            if hasattr(node, "set_params"):
+                node.set_params(**node_params)
+            else:
+                if node_params == {}:
+                    continue  # No parameters to set, skip
+                message = f"Node '{node_name}' does not have a 'set_params' method, cannot set parameters {node_params}."
+                raise ValueError(message)
+
+    def get_sink_node_name(self) -> str:
+        """Get the name of the sink node in the pipeline."""
+        for node_name, (_, node_config) in self._config.nodes.items():
+            if node_config.node_type == NodeType.SINK:
+                return node_name
+        message = "No sink node found in the pipeline."
+        raise ValueError(message)
+
+    # --- Internal methods for object instantiation
 
     def _instantiate_nodes(self) -> None:
         """Instantiate node objects from the pipeline configuration."""
-        self.node_objects = {}
         for node_name, (node_class_name, node_config) in self._config.nodes.items():
             node_class = NODE_REGISTRY[node_class_name]["node_class"]
             self.node_objects[node_name] = node_class(config=node_config)
 
+    def _instantiate_sink_ports(self) -> None:
+        """Instantiate sink ports by connecting them to the appropriate nodes based on the edges configuration."""
+        incoming_edges_idx: list[int] = [] # List of indices of edges that are incoming to sink nodes, used to identify which nodes are connected to the sink ports.
+        for idx, edge in enumerate(self._config.edges):
+            if edge.target in self._config.nodes and self._config.nodes[edge.target][1].node_type == NodeType.SINK:
+                incoming_edges_idx.append(idx)
+
+        for idx in incoming_edges_idx:
+            edge = self._config.edges[idx]
+            sink_node_name = edge.target
+            source_node_name = edge.source
+            # Connect the source node's output port to the sink node's "dump" input port
+            original_port = edge.ports_map["dump"]
+            self._config.edges[idx].ports_map = {f"{source_node_name}:{original_port}": original_port}
+            # Add this port to the sink node's ports
+            self.node_objects[sink_node_name].add_port(port_name=f"{source_node_name}:{original_port}")
     # --- Internal methods for graph management
 
     @decompile
@@ -236,6 +301,12 @@ class Pipeline:
         """Initialize the internal graph representation using networkx."""
         self._graph: nx.DiGraph[str] = nx.DiGraph()
         self._compiled = False
+
+    @decompile
+    def _init_metric_edges(self) -> None:
+        """Initialize metric edges in the internal graph representation."""
+        for edge in self._config.metric_edges:
+            self._add_edge_to_repr(edge)
 
     @decompile
     def _init_nodes(self) -> None:
@@ -264,8 +335,11 @@ class Pipeline:
                 # Remove from the objects
                 del self.node_objects[node_name]
 
-    def _add_node_to_repr(self, node_name: str, node_class_name: str, node_config: NodeConfig) -> None:
+    def _add_node_to_repr(self, node_name: str, node_class_name: str, node_config: NodeConfig | None) -> None:
         """Add a node to the internal graph representation."""
+        if node_config is None:
+            node_config = NODE_REGISTRY[node_class_name]["node_config_class"]()  # Use default config if None is provided
+            self._config.nodes[node_name] = (node_class_name, node_config)  # Update the config with the default config
         features = node_config.model_dump() # TODO : Maybe not all features need to be added as node attributes, since its mostly display purposes.
         features.update({"node_class_name": node_class_name})
         self._graph.add_node(node_for_adding=node_name, **features)
@@ -277,6 +351,27 @@ class Pipeline:
             v_of_edge=edge.target,
             ports_map=edge.ports_map,
         )
+
+    def _add_metric_edge_to_repr(self, edge: Edge) -> None:
+        """Add a metric edge to the internal graph representation."""
+        self._graph.add_edge(
+            u_of_edge=edge.source,
+            v_of_edge=edge.target,
+            ports_map=edge.ports_map,
+            is_metric_edge=True,  # Add an attribute to differentiate metric edges from regular edges
+        )
+
+    def remove_metric_edge_from_repr(self, source: str, target: str) -> None:
+        """Remove a metric edge from the internal graph representation."""
+        if self._graph.has_edge(source, target):
+            if self._graph.edges[source, target].get("is_metric_edge", False):
+                self._graph.remove_edge(u=source, v=target)
+            else:
+                message = f"Edge from '{source}' to '{target}' exists but is not a metric edge."
+                raise ValueError(message)
+        else:
+            message = f"Edge from '{source}' to '{target}' does not exist in the pipeline."
+            raise ValueError(message)
 
     def _remove_node_from_repr(self, node_name: str) -> None:
         """Remove a node from the internal graph representation."""
@@ -291,6 +386,16 @@ class Pipeline:
     def _validate(self) -> None:
         """Validate the pipeline layout, ensure the graph is directed and acyclic."""
         valid = nx.is_directed_acyclic_graph(self._graph)
+        # Check that there's only one sink node
+        sink_nodes = [n for n, n_conf in self._config.nodes.items() if n_conf[1].node_type == NodeType.SINK]
+        if len(sink_nodes) > 1:
+            valid = False
+            message = f"Pipeline has multiple sink nodes: {sink_nodes}. Only one sink node is allowed."
+            raise ValueError(message)
+        if len(sink_nodes) == 0:
+            valid = False
+            message = f"Pipeline has no sink node. At least one sink node is required."
+            raise ValueError(message)
         if not valid:
             message = "The pipeline graph must be a directed acyclic graph (DAG).\n"
             message += (
@@ -311,6 +416,9 @@ class Pipeline:
 
         # If everything is valid, instantiate the nodes from the config to prepare for execution.
         self._instantiate_nodes()
+
+        # Instantiate sink ports
+        self._instantiate_sink_ports()
 
         self._compiled = True
 
